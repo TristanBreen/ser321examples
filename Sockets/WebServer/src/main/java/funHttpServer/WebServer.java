c90a7dc1d90a72cdfebe6952c6634546f1866251
TristanBreen <tristan.a.breen@gmail.com>
2024-02-12 17:21:10 -0700

    commit

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -203,8 +203,8 @@ class WebServer {
 
           // extract required fields from parameters
           
-            Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-            Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+            Integer num1 = Integer.parseInt(query_pairs.get("num1","0"));
+            Integer num2 = Integer.parseInt(query_pairs.get("num2","0"));
 
             // do math
             String result = Integer.toString(num1 * num2);
